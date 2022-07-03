3d30aec66f7eb7358e890fdcdd421e7d0d1b4bf7
lucas <lpatronr@gmail.com>
2022-07-03 18:25:44 -0500

    feat: enable cookie parser, cors, and set the global prefix to 'api'

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
 
 const PORT = 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cookieParser());
+  app.enableCors({
+    methods: 'GET,PUT,POST,PATCH,DELETE',
+    credentials: true,
+    origin: 'http://localhost:3000',
+  });
+  app.setGlobalPrefix('api');
   await app.listen(PORT);
 }
-bootstrap();
+bootstrap().then(() => {
+  console.log(`Server running on port ${PORT}`);
+});
