b4821432fd0748e49f4648162c1ea435a81ac21b
lucas <lpatronr@gmail.com>
2022-07-03 17:45:47 -0500

    docs: add descriptions for missing commit types

--- .commitlint.config.js
+++ .commitlint.config.js
@@ -1,10 +1,12 @@
 // build: Changes that affect the build system or external dependencies (example scopes: gulp, broccoli, npm)
+// chore: Updating grunt tasks etc; no production code change
 // ci: Changes to our CI configuration files and scripts (example scopes: Travis, Circle, BrowserStack, SauceLabs)
 // docs: Documentation only changes
 // feat: A new feature
 // fix: A bug fix
 // perf: A code change that improves performance
 // refactor: A code change that neither fixes a bug nor adds a feature
+// revert: Revert to a previous commit
 // style: Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)
 // test: Adding missing tests or correcting existing tests
 
@@ -21,11 +23,7 @@ module.exports = {
     'footer-max-line-length': [error, 'always', maxLength],
     'header-max-length': [error, 'always', maxLength],
     'scope-case': [error, 'always', 'lower-case'],
-    'subject-case': [
-      error,
-      'never',
-      ['sentence-case', 'start-case', 'pascal-case', 'upper-case'],
-    ],
+    'subject-case': [error, 'never', ['sentence-case', 'start-case', 'pascal-case', 'upper-case']],
     'subject-empty': [error, 'never'],
     'subject-full-stop': [error, 'never', '.'],
     'type-case': [error, 'always', 'lower-case'],
